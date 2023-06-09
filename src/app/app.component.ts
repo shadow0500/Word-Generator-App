8d95639cf2607250893d2d64ff45cce074e7dad0
Shadmaan <shadmaank2601@gmail.com>
2023-06-09 20:27:15 +0530

    Push after update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import arrayWords from "../assets/words";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+
+  words='';
+  limit=20;
+
+
+
+  handleSlideChange(newLimit:number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words=arrayWords.slice(0,this.limit).join(' ');
+  }
+
+
 }
